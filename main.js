ad39369158435b94920d9c362731b2af9b56c888
Luke Turner <github@luketurner.org>
2022-04-05 10:13:52 -0700

    experiment with some stuff

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function drawInPerspective() {
   drawHorizon(ctx);
   if (globalState.type === '1p') {
     drawVanishingPoint(ctx, 1);
-    drawRect1P(ctx, 350, 600, 100, 100);
+    drawRect1P(ctx, 350, 600, 100);
   } else {
     drawVanishingPoint(ctx, 1);
     drawVanishingPoint(ctx, 2);
@@ -29,12 +29,32 @@ function drawInPerspective() {
 
 // Drawing functions (using global state)
 
-function drawRect1P(ctx, x, y, width, height) {
-  rect(ctx, x, y, width, height);
-  drawVanishingLine(ctx, x,         y,          1);
-  drawVanishingLine(ctx, x,         y + height, 1);
-  drawVanishingLine(ctx, x + width, y,          1);
-  drawVanishingLine(ctx, x + width, y + height, 1);
+function drawRect1P(ctx, x, y, width) {
+  rect(ctx, x, y, width, width);
+  drawVanishingLine(ctx, x,         y,         1);
+  drawVanishingLine(ctx, x,         y + width, 1);
+  drawVanishingLine(ctx, x + width, y,         1);
+  drawVanishingLine(ctx, x + width, y + width, 1);
+  
+  // NOTE -- this width should be reduced by something or other, since it's foreshortened
+  moveToVanishingPointAndDot(ctx, x,         y,         width, 1);
+  moveToVanishingPointAndDot(ctx, x + width, y,         width, 1);
+  moveToVanishingPointAndDot(ctx, x,         y + width, width, 1);
+  moveToVanishingPointAndDot(ctx, x + width, y + width, width, 1);
+  moveToVanishingPointAndDot2(ctx, x,         y,         0.25, 1);
+  moveToVanishingPointAndDot2(ctx, x + width, y,         0.25, 1);
+  moveToVanishingPointAndDot2(ctx, x,         y + width, 0.25, 1);
+  moveToVanishingPointAndDot2(ctx, x + width, y + width, 0.25, 1);
+}
+
+function moveToVanishingPointAndDot(ctx, x, y, len, vpIx) {
+  const [ex, ey] = moveToVanishingPoint(x, y, len, vpIx);
+  dot(ctx, ex, ey);
+}
+
+function moveToVanishingPointAndDot2(ctx, x, y, plen, vpIx) {
+  const [ex, ey] = moveToVanishingPoint2(x, y, plen, vpIx);
+  dot(ctx, ex, ey);
 }
 
 function drawRect2P(ctx, x, y, h) {
@@ -45,8 +65,39 @@ function drawRect2P(ctx, x, y, h) {
   drawVanishingLine(ctx, x, y + h, 2);
 }
 
+function moveToVanishingPoint2(x, y, plen, vpIx) {
+  const [vx, vy] = vpCoords(vpIx);
+  const rx = (vx - x) * plen;
+  const ry = (vy - y) * plen;
+  return [x + rx, y + ry];
+}
+
+/**
+ * Starting at given x, y coordinate pair, "walks toward" the given vanishing point for LEN pixels.
+ * Returns the resulting coordinate pair as an array [newx, newy]
+ */
+function moveToVanishingPoint(x, y, len, vpIx) {
+  const [vx, vy] = vpCoords(vpIx);
+  // Note: theta is the angle (in radians) from the given point to the vanishing point,
+  // where an angle of 0 means the vanishing point has the same Y-coordinate (i.e. the line is horizontal).
+  // But, if (vx - x) is negative, the line should go in the opposite direction, so we need to
+  // "flip" the angle about the origin by adding PI radians to the value.
+  const theta = Math.atan((vy - y) / (vx - x)) + (vx - x < 0 ? Math.PI : 0);
+  const dx = Math.cos(theta) * len;
+  const dy = Math.sin(theta) * len;
+  return [x + dx, y + dy];
+}
+
+function vpCoords(ix) {
+  return [globalState[`vp${ix}X`], globalState.horizonY];
+}
+
 function drawVanishingLine(ctx, x, y, vpIx) {
   line(ctx, x, y, globalState[`vp${vpIx}X`], globalState.horizonY);
+}
+
+function angleToVanishingPoint(x, y, vpIx) {
+  return pointsAngle(x, y, globalState[`vp${vpIx}X`], globalState.horizonY);
 }
 
 function drawHorizon(ctx) {
@@ -58,6 +109,19 @@ function drawVanishingPoint(ctx, ix) {
 }
 
 // lower-level drawing functions (not using global state)
+
+function pointsAngle(x1, y1, x2, y2) {
+  return Math.tan(y2 - y1 / x2 - x1);
+}
+
+function lineAtAngle(ctx, x1, y1, angle, len) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x1 + len, y1);
+  ctx.rotate(angle);
+  ctx.stroke();
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
 
 function rect(ctx, x, y, width, height) {
   ctx.beginPath();
