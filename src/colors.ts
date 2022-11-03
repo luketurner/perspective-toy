32b066e5bae7b6e857f1727c93afb9322e5d432e
Luke Turner <github@luketurner.org>
2022-11-02 19:07:17 -0700

    adjust colors

--- src/colors.ts
+++ src/colors.ts
@@ -1,6 +1,6 @@
-export const colorFore = '#78716c';
-export const colorSubtle = '#d6d3d1';
-export const colorHover = '#4d7c0f';
-export const colorDragging = '#a16207';
-export const colorBlue = '#6366f1';
-export const colorDarkBlue = '#4338ca';
+export const     colorFore = '#44403c'; // stone-700
+export const   colorSubtle = '#a8a29e'; // stone-400
+export const    colorHover = '#4d7c0f'; // lime-700
+export const colorDragging = '#4d7c0f'; // lime-700
+export const     colorBlue = '#6366f1'; // indigo-500
+export const colorDarkBlue = '#4338ca'; // indigo-700
